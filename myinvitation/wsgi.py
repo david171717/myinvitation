330490061e6d5125eb076f2533cf2717a60bef92
David <>
2019-10-26 17:03:34 +0900

    wsgi.py 수정

--- myinvitation/wsgi.py
+++ myinvitation/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "invitation.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myinvitation.settings")
 
 application = get_wsgi_application()
